5af625f00d4f24ca747bcf54eb0ccd65b6f47c02
Bea Boxler <bea@boxler.me>
2025-04-20 08:14:24 +0000

    Update init to install tpm

--- init.sh
+++ init.sh
@@ -4,3 +4,7 @@ cd $HOME
 git clone https://github.com/BeesBoxler/.dotfiles.git
 cd .dotfiles
 ./setup.sh
+
+git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
+
+echo "Please ensure you have tmux 3.4 and nvim 0.11 installed. Peace out."
